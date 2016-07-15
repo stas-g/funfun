6bef9a0be8d7b1ba965617bf175d7ddd0c006875
Nastasiya Grinberg <ng414@mdpc244.medschl.cam.ac.uk>
2016-07-15 18:10:27 +0100

    commit

--- R/maxz.R
+++ R/maxz.R
@@ -1,5 +1,5 @@
 #' The \eqn{f(x) = max\{x, 0\}} function
-#' @param x Any numeric/integer object
+#' @param x Any numeric object
 #' @return A maximum of the argument, capped at 0.
 #' @author N. F. Grinberg, \email{ng414@@medsch.cam.ac.uk}
 #' @keywords max
@@ -11,5 +11,5 @@
 #' @export
 
 maxz <- function(x){
-	sapply(x, FUN = function(z) ifelse(z > 0, z, 0))
+  ifelse(x > 0, x, 0)
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # funfun
 # funfun
+# funfun
