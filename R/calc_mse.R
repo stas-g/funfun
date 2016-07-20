9f708284eea04823b295e1fce7823414cf2fdbd5
Nastasiya Grinberg <ng414@mdpc244.medschl.cam.ac.uk>
2016-07-20 13:44:07 +0100

    one moar try

---
+++ R/calc_mse.R
@@ -0,0 +1,20 @@
+#' Calculate mean squared error for a vector of observed and predicted values
+#' @param obs Stuff stuff
+#' @param pred Stuff stuff
+#' @param rsq Stuff stuff
+#' @return mse or R^2
+#' @author N. F. Grinberg, \email{ng414@medschl.cam.ac.uk}
+#' @keywords mse
+#' @export
+#' @examples
+#'
+#' @export
+
+calc.mse <- function(obs, pred, rsq = FALSE){
+  if(!is.matrix(obs) | !is.matrix(pred)){obs <- as.matrix(obs); pred <- as.matrix(pred)}
+  rs <- (obs - pred)^2
+  if(rsq == FALSE){colMeans(rs, na.rm = TRUE)}else{
+    meanmat <- matrix(rep(colMeans(obs, na.rm = TRUE), nrow(obs)), nrow = nrow(obs), byrow = TRUE)
+    1 - colSums(rs, na.rm = TRUE)/colSums((obs - meanmat)^2, na.rm = TRUE)
+  }
+}
