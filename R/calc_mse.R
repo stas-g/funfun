72d3eae0566cc49897880b070665f7b13605a5d9
stas-g <ng414@medschl.cam.ac.uk>
2016-08-05 16:35:03 +0100

    updated calc.mse

--- R/calc_mse.R
+++ R/calc_mse.R
@@ -1,20 +1,28 @@
-#' Calculate mean squared error for a vector of observed and predicted values
-#' @param obs Stuff stuff
-#' @param pred Stuff stuff stuff
-#' @param rsq Stuff stuff stuff
-#' @return mse or R^2 and stuff
+#' Calculate mean squared error (MSE) for a vector or a matrix of observed and predicted values
+#'
+#' @param obs A vector or a matrix/df (samples in rows, individual variables in columns) of observed values
+#' @param pred A vector or a matrix/df (samples in rows, individual variables in columns) of predicted values
+#' @param rsq Calculate \eqn{R^2} (R-squared) instead of MSE? Defualt FALSE
+#'
+#' @return MSE or $R^2$ (single value if vectors are passed or a vector of length \code{nrow(obs)} if matrices/df are passed, one value for each obs-pred column pair
 #' @author N. F. Grinberg, \email{ng414@medschl.cam.ac.uk}
-#' @keywords mse
-#' @export
+#' @keywords MSE, Rsq
+#'
 #' @examples
+#' mod <- lm(mpg ~ ., data = mtcars)
+#' summary(mod)$r.squared
+#' calc.mse(mtcars$mpg, mod$fitted.values, rsq = TRUE)
 #'
 #' @export
 
 calc.mse <- function(obs, pred, rsq = FALSE){
-  if(!is.matrix(obs) | !is.matrix(pred)){obs <- as.matrix(obs); pred <- as.matrix(pred)}
-  rs <- (obs - pred)^2
-  if(rsq == FALSE){colMeans(rs, na.rm = TRUE)}else{
-    meanmat <- matrix(rep(colMeans(obs, na.rm = TRUE), nrow(obs)), nrow = nrow(obs), byrow = TRUE)
-    1 - colSums(rs, na.rm = TRUE)/colSums((obs - meanmat)^2, na.rm = TRUE)
+  if(is.vector(obs)) obs <- as.matrix(obs)
+  if(is.vector(pred)) pred <- as.matrix(pred)
+
+  n <- nrow(obs)
+  rss <- colSums((obs - pred)^2, na.rm = TRUE)
+  if(rsq == FALSE) rss/n else {
+    tss <- diag(var(obs, na.rm = TRUE)) * (n - 1)
+    1 - rss/tss
   }
 }
